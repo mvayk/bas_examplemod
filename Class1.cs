be5373d45a641657ddda54706d1e803b6d26a906
mvayк <45300955+mvayk@users.noreply.github.com>
2023-12-30 22:52:27 +1100

    Blade & Sorcery U11 Example Code Mod

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ThunderRoad;
+using UnityEngine;
+
+namespace bas_examplemod;
+
+public class Main : ThunderScript {
+    public override void ScriptLoaded(ModManager.ModData modData) {
+      base.ScriptLoaded(modData);
+      EventManager.onLevelLoad += new EventManager.LevelLoadEvent(this.EventManager_onLevelLoad);
+      Debug.Log("B&S Example Mod Has Loaded");
+    }
+
+    private void EventManager_onLevelLoad(LevelData levelData, EventTime eventTime) {
+      Debug.Log("Level Loaded");
+    }
+}
